734196aaae52071cea9eb8215db1fdc559bbca7f
Hossam-MHMAD <hossamtulimat2009@gmail.com>
2025-04-10 17:37:27 +0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,6 +3,8 @@ class Player:
     self.x = x
     self.y = y
     self.player_speed = 5
+    self.health = 10
+    self.fire_list = []
 
   def Move_Right(self):
     self.x += self.player_speed
@@ -15,3 +17,21 @@ class Player:
 
   def Move_Down(self):
     self.y += self.player_speed
+  
+  def Take_Damage(self):
+    self.health -= 1
+
+  # Bullet_Direction Here Is To determine which player has shoot if left player 1 or -1 if right player, 
+  # and Screen_Width is to check if the bullet get out of the screen
+  # and i used the Bullet_Direction To To Determine Which Player i will remove its bullet if it gets out of the screen
+  def Move_Bullets(self, Bullet_Direction, Screen_Width): 
+    for bullet in self.fire_list:
+      bullet.x += Bullet_Direction
+
+      if Bullet_Direction > 0: # For Left Player Bullets
+        if bullet.x > Screen_Width:
+          self.fire_list.remove(bullet)
+      
+      else: # For Right Player Bullets
+        if bullet.x < 0:
+          self.fire_list.remove(bullet)
